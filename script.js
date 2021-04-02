19442fbcf90ef175d772cb6c00fb42ed337472e9
Boris Sizov <bsizow@yandex.ru>
2021-04-02 03:20:36 +0300

    feat: добавлять карточки из массива js

--- script.js
+++ script.js
@@ -1,14 +1,76 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsList = document.querySelector('.card-list');
+const plug = document.querySelector('#plug').content;
+const card = document.querySelector('#card').content;
+
+if(initialCards.length === 0) {
+
+  cardsList.prepend(plug);
+
+} else if (initialCards.length !== 0) {
+
+
+  const cardsListArr = initialCards.map((item) => {
+
+    const newCard = card.cloneNode(true);
+
+    const newCardImage = newCard.querySelector('.card__image');
+    const newCardDescription = newCard.querySelector('.card__description');
+
+    newCardImage.setAttribute('src', item.link);
+    newCardImage.setAttribute('alt', item.name);
+    newCardDescription.textContent = item.name;
+
+    return newCard;
+  });
+
+  cardsList.prepend(...cardsListArr);
+
+}
+
+// если card-list или initialCards пустой - вывести plug
+
+// если initialCards что-то содержить - добавить на страницу
+
+// также при удалении всех карточек выводить plug
+
 // инофрмация о пользователе
-let userName = document.querySelector('.profile__name');
-let userAbout = document.querySelector('.profile__about');
+const userName = document.querySelector('.profile__name');
+const userAbout = document.querySelector('.profile__about');
 // кнопка редактирования
-let editButton = document.querySelector('.profile__button_type_edit');
-// popup Edit
-let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__button_type_close');
-let inputName = popup.querySelector('.popup__input_type_name');
-let inputAbout = popup.querySelector('.popup__input_type_about');
-let formEdit = popup.querySelector('.popup__form_type_edit');
+const editButton = document.querySelector('.profile__button_type_edit');
+// popup
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__button_type_close');
+const inputName = popup.querySelector('.popup__input_type_name');
+const inputAbout = popup.querySelector('.popup__input_type_about');
+const formEdit = popup.querySelector('.popup__form_type_edit');
 
 // функция открытия popup
 function openPopup() {
